7fb155e98d19aa4683c6ecfefa2aa8649d196af6
Anurag yadav <anuragyadav622003@gmail.com>
2024-09-12 22:49:40 +0530

    changes

--- E-learning-frontend/src/components/Auth/Login.jsx
+++ E-learning-frontend/src/components/Auth/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function Login() {
-  const { loginWithRedirect,user } = useAuth0();
-console.log(user);
+  const { loginWithRedirect } = useAuth0();
+
   const handleGoogleLogin = () => {
     // Trigger the Auth0 login with the Google connection
     loginWithRedirect({ connection: 'google-oauth2' });
