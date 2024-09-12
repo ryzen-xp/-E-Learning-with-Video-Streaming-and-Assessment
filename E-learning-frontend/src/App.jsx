21a9a575a807b8a22f17359036c7e230f3482219
Anurag yadav <anuragyadav622003@gmail.com>
2024-09-13 00:05:44 +0530

    changes

--- E-learning-frontend/src/App.jsx
+++ E-learning-frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Profile from './components/Auth/Profile'
 function App() {
   return (
     <div className='h-screen grid place-content-center'>
-      <Login/>
+      {/* <Login/> */}
       {/* <Signup/> */}
       <Profile/>
     </div>
