ddf88cafd8ab4e321ef40cb7740569496c45cd95
Anurag yadav <134534151+Anuragyadav622003@users.noreply.github.com>
2024-09-15 21:55:43 +0530

    Update index.js

--- E-learning-backend/index.js
+++ E-learning-backend/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './db.js';
-import { sum } from './src/controllers/UserController.js';
-import router from './src/routers/User.js';
+
 
 
 
@@ -17,11 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-console.log(sum(20,30))
+
 app.get('/',(req,res)=>{
   res.send('hello world')
 })
-app.use('/api/users',router); // Use the user routes
+
 
 // Start server
 app.listen(PORT, () => {
