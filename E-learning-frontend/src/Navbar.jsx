978e28290c6b58bf93ea8609dbbab05d966d9ced
Anurag yadav <anuragyadav622003@gmail.com>
2024-09-17 23:57:57 +0530

    change

--- E-learning-frontend/src/Navbar.jsx
+++ E-learning-frontend/src/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
   const classNames = (...classes) => classes.filter(Boolean).join(' ');
 
   return (
-    <nav className="bg-gray-800">
+    <nav className="bg-gray-800 w-screen">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
