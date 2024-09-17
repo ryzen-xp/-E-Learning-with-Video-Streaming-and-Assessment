254bb16a91788819aa2bd279c44424b12922d809
Anurag yadav <anuragyadav622003@gmail.com>
2024-09-17 23:52:23 +0530

    change

--- E-learning-frontend/src/components/Home/Home.jsx
+++ E-learning-frontend/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 
 const Signup = () => {
   return (
-    <div className="flex flex-col  bg-base-100 items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col  bg-base-100 items-center justify-center min-h-screen  p-4">
       <Banner className="w-full mb-10" /> {/* Full width and margin below the banner */}
       <div className="w-full mt-10">
         <Footer className="mt-10 bg-gray-800 text-white py-4 text-center" /> {/* Dark background, white text, padding */}
