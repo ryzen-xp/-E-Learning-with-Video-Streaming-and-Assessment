a91bfcf9e46c78d432de92ef81ba78ca1fbd55f6
Anurag yadav <anuragyadav622003@gmail.com>
2024-09-21 16:17:47 +0530

    change

--- E-learning-frontend/src/components/Home/Banner.jsx
+++ E-learning-frontend/src/components/Home/Banner.jsx
@@ -5,7 +5,7 @@ const banner = "/E.jpg";
 function Banner() {
   return (
     <>
-      <div className=" max-w-screen-2xl  container mx-auto px-20 md:px-20 sm:px-4 flex flex-col md:flex-row  min-h-screen">
+      <div className=" max-w-screen-2xl  container mx-auto px-20 md:px-20 sm:px-4 flex flex-col md:flex-row h-screen">
         <div className="w-full order-2 px-10 md:order-1 md:w-1/2 mt-12 md:mt-36 max-h-screen">
           <div className="space-y-8 ">
             <h1 className="text-xl md:text-3xl font-bold text-white">
