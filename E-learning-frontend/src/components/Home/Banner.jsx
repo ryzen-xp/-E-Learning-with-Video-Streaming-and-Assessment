224a5b3397b8090f53e636b0866e2ea88adf1104
Anurag yadav <anuragyadav622003@gmail.com>
2024-09-22 14:11:08 +0530

    ch

--- E-learning-frontend/src/components/Home/Banner.jsx
+++ E-learning-frontend/src/components/Home/Banner.jsx
@@ -5,11 +5,11 @@ function Banner() {
     <div className="flex flex-col-reverse md:flex-row items-center justify-between max-w-screen-2xl container mx-auto px-4 md:px-20 lg:px-24 py-16 md:h-screen">
       {/* Left Text Section */}
       <div className="w-full md:w-1/2 space-y-6 md:space-y-10 text-white md:pr-10">
-        <h1 className="text-3xl md:text-5xl font-extrabold leading-tight">
+        <h1 className="text-4xl md:text-6xl font-extrabold leading-tight">
           Welcome! Learn something{" "}
           <span className="text-pink-400">new every day!</span>
         </h1>
-        <p className="text-lg md:text-xl text-gray-300">
+        <p className="text-lg md:text-2xl text-gray-300">
           Explore a variety of topics and expand your knowledge with engaging
           content and resources.
         </p>
@@ -35,7 +35,7 @@ function Banner() {
       </div>
 
       {/* Right Image Section */}
-      <div className="w-full md:w-1/2 flex justify-center items-start mt-6 md:mt-0 pb-4 md:ml-12">
+      <div className="w-full md:w-1/2 flex justify-center items-start mt-6 md:mt-0 sm:pb-16 md:ml-12">
         <img
           src='/Banner.svg' // Ensure the path is correct
           className="h-auto max-h-full w-full md:min-h-[450px] object-contain rounded-lg shadow-lg"
